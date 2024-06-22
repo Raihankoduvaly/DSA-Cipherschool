0cf3908ea9dc7279a644885ad9c1493a0a4715ac
Raihan Koduvaly <119101777+Raihankoduvaly@users.noreply.github.com>
2024-06-22 10:36:12 +0530

    Add files via upload

---
+++ lecture7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e;
+
+    cout<<"give five integers as input:"<<endl;
+    cin>>a>>b>>c>>d>>e;
+
+    cout<<"the integers in reverse order are:"<<e<<d<<c<<b<<a;
+
+    return 0;
+
+
+}
