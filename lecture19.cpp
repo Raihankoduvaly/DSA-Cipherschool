a49d409a146411d6c1b84dcbf2e7fa943b1dbfbc
Raihan Koduvaly <119101777+Raihankoduvaly@users.noreply.github.com>
2024-07-13 13:20:17 +0530

    Add files via upload

---
+++ lecture19.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long int(a);
+    int(b);
+
+    cout<<"the size of long long int:  "<<sizeof(a)<<endl;
+    cout<<"the size of int:  "<<sizeof(b)<<endl;
+
+   return 0;
+}
